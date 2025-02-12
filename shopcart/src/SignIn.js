70e4e851c124c88fe7927c5539d4657b9e907cbf
harperers <max@pan-ami.com>
2025-02-12 13:10:02 -0800

    facebook login finished

--- shopcart/src/SignIn.js
+++ shopcart/src/SignIn.js
@@ -1,11 +1,13 @@
+/* global FB */
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import FacebookLogin from "react-facebook-login";
 
 const SignIn = () => {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [facebookUser, setFacebookUser] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     window.fbAsyncInit = function () {
@@ -30,6 +32,7 @@ const SignIn = () => {
   const handleLogin = (e) => {
     e.preventDefault();
     console.log("login:", { name, password });
+    navigate("/checkout", { state: { username: name } });
   };
 
   const responseFacebook = (response) => {
@@ -37,6 +40,7 @@ const SignIn = () => {
     if (response.name) {
       setFacebookUser(response);
       setName(response.name);
+      navigate("/checkout", { state: { username: response.name } });
     }
   };
 
@@ -46,11 +50,12 @@ const SignIn = () => {
       <p>Please login with one of the following:</p>
       <div>
         <FacebookLogin
-          appId="your-facebook-app-id"
+          appId="615752191178833"
           fields="name,email,picture"
           callback={responseFacebook}
           icon="fa-facebook"
           textButton="Login with Facebook"
+          scope="email"
         />
       </div>
       <div>OR</div>
@@ -82,17 +87,15 @@ const SignIn = () => {
             style={{ padding: "8px", width: "100%" }}
           />
         </div>
-        <Link to="/checkout" state={{ username: name }}>
-          <button
-            type="submit"
-            style={{
-              marginTop: 20,
-              width: "100%",
-            }}
-          >
-            Login
-          </button>
-        </Link>
+        <button
+          type="submit"
+          style={{
+            marginTop: 20,
+            width: "100%",
+          }}
+        >
+          Login
+        </button>
       </form>
       <div style={{ marginTop: "20px" }}>
         <Link to="/">
