07d52a9ce216cc5f9f66fab7bdc0722c2835522a
harperers <max@pan-ami.com>
2025-02-12 12:55:02 -0800

    facebook app id

--- shopcart/src/SignIn.js
+++ shopcart/src/SignIn.js
@@ -1,13 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import FacebookLogin from "react-facebook-login";
 
 const SignIn = () => {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
+  const [facebookUser, setFacebookUser] = useState(null);
+
+  useEffect(() => {
+    window.fbAsyncInit = function () {
+      FB.init({
+        appId: "615752191178833",
+        autoLogAppEvents: true,
+        xfbml: true,
+        version: "v12.0",
+      });
+    };
+
+    (function (d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
+  }, []);
 
   const handleLogin = (e) => {
     e.preventDefault();
     console.log("login:", { name, password });
+  };
+
+  const responseFacebook = (response) => {
+    console.log("Facebook response: ", response);
+    if (response.name) {
+      setFacebookUser(response);
+      setName(response.name);
+    }
   };
 
   return (
@@ -15,7 +45,13 @@ const SignIn = () => {
       <h2>Sign In</h2>
       <p>Please login with one of the following:</p>
       <div>
-        <button>Login with Facebook</button>
+        <FacebookLogin
+          appId="your-facebook-app-id"
+          fields="name,email,picture"
+          callback={responseFacebook}
+          icon="fa-facebook"
+          textButton="Login with Facebook"
+        />
       </div>
       <div>OR</div>
       <form
